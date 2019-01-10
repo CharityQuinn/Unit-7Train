95484dff8892d1b0343c03414ce3322f8a9ff5fc
CharityQuinn <charityaquinn@gmail.com>
2019-01-10 15:14:11 -0500

    Working edit and delete buttons, Yea!

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -14,6 +14,8 @@ firebase.initializeApp(config);
 // save firebase database reference
 var database = firebase.database();
 
+var activeTrainKey = "";
+
 
 // add event listener for form submit
 $("#submit-btn").on("click", function (event) {
@@ -33,7 +35,12 @@ $("#submit-btn").on("click", function (event) {
     frequency: trainFrequency
 
   };
-  database.ref().push(newTrain);
+  if (activeTrainKey) {
+    database.ref(activeTrainKey).set(newTrain);
+    activeTrainKey = "";
+  } else {
+    database.ref().push(newTrain);
+  }
 
 
   // clear out any value in form input tags on page
@@ -73,8 +80,78 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
   nextTrain = (moment(nextTrain).format("hh:mm A"));
 
   // Populate the current train data in html, create table to do it
-  $("#train-Table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
-    trainFrequency + " min" + "</td><td>" + nextTrain + "</td><td>" + "Arrives in : " + tMinutesTillTrain + " min" + "</td>" + "<td>" + "<button class='btn btn-secondary'>" + "<i class='fas fa-file-signature'></i>" + "</button>" +
-    "<button 'class=''btn btn-secondary'>" + "<i class='fas fa-undo'>" + "</i>" + "</button>" + "</td>" + "</tr>"
+  $("#train-Table > tbody").append("<tr data-key=" + childSnapshot.key + "><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+    trainFrequency + " min" + "</td><td>" + nextTrain + "</td><td>" + "Arrives in : " + tMinutesTillTrain + " min" + "</td>" + "<td>" + "<button  data-key=" + childSnapshot.key + "  class='btn btn-secondary edit'>" + "<i class='fas fa-file-signature'></i>" + "</button>" +
+    "<button  data-key=" + childSnapshot.key + " class='btn btn-secondary delete'>" + "<i class='fas fa-undo'>" + "</i>" + "</button>" + "</td>" + "</tr>"
   );
+});
+
+database.ref().on("child_changed", function (childSnapshot, prevChildKey) {
+
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var time = childSnapshot.val().firstTrainTime;
+  var trainFrequency = childSnapshot.val().frequency;
+  // start the time process and functioning
+  var tFrequency = trainFrequency;
+
+  var firstTime = time;
+
+  var firstTimeConverted = moment(firstTime, "HH:mm");
+
+  var currentTime = moment();
+
+
+  var diffTime = parseInt(moment().diff(moment(firstTimeConverted), "minutes"));
+
+  // Set a variable for how frequently the train arrives
+  var tRemainder = parseInt(diffTime) % parseInt(tFrequency);
+
+  //Convert the time frequency into a number
+  tMinutesTillTrain = parseInt(tFrequency) - parseInt(tRemainder);
+  // Set variable for getting the time to the next train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  nextTrain = (moment(nextTrain).format("hh:mm A"));
+
+  // Populate the current train data in html, create table to do it
+  $(`tr[data-key=${childSnapshot.key}]`).html("<td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+    trainFrequency + " min" + "</td><td>" + nextTrain + "</td><td>" + "Arrives in : " + tMinutesTillTrain + " min" + "</td>" + "<td>" + "<button  data-key=" + childSnapshot.key + "  class='btn btn-secondary edit'>" + "<i class='fas fa-file-signature'></i>" + "</button>" +
+    "<button  data-key=" + childSnapshot.key + " class='btn btn-secondary delete'>" + "<i class='fas fa-undo'>" + "</i>" + "</button>" + "</td>"
+  );
+});
+$(document).ready(function () {
+
+  $(document).on("click", ".delete", function (event) {
+    // retrieve key from button
+    activeTrainKey = $(this).attr("data-key");
+    console.log("Delete button clicked");
+    database.ref(activeTrainKey).remove().then(function() {
+      // remove row after successful deletion 
+      $(`tr[data-key=${activeTrainKey}]`).remove();
+      activeTrainKey = "";
+    })
+    
+
+  });
+
+
+  $(document).on("click", ".edit", function() {
+    // get key out of button
+    activeTrainKey = $(this).attr("data-key");
+    database.ref(activeTrainKey).once("value", function(childSnapshot) {
+      // retrieve values out of snapshot.val()
+      console.log("inside edit button");
+      //  trainName = childSnapshot.val().name;
+      //  trainDestination = childSnapshot.val().destination;
+      //  time = childSnapshot.val().firstTrainTime;
+      //  trainFrequency = childSnapshot.val().frequency;
+      // write values to form input tags
+      // $("#train-name-input").val(childSnapshot.val().trainName);
+      console.log(childSnapshot.val());
+      $("#train-input").val(childSnapshot.val().name);
+      $("#destination-input").val(childSnapshot.val().destination);
+      $("#firstStart-input").val(childSnapshot.val().firstTrainTime);
+      $("#frequency-input").val(childSnapshot.val().frequency);
+    })
+  })
 });
